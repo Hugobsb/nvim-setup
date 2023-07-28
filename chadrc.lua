a9134dc38e43acbf5f5710f0fba3fa5ba66e8316
Hugo B. S. Benedicto <hugo.bsbenedicto@gmail.com>
2023-07-27 21:28:35 -0300

    refactor: ui configuration

--- chadrc.lua
+++ chadrc.lua
@@ -2,7 +2,18 @@
 
 local M = {}
 
-M.ui = { theme = "chadracula", theme_toggle = { "chadracula", "one_light" } }
+M.ui = {
+  theme = "chadracula",
+  theme_toggle = { "chadracula", "one_light" },
+
+  nvdash = {
+    load_on_startup = true,
+  },
+
+  statusline = {
+    theme = "vscode_colored",
+  },
+}
 
 M.mappings = require "custom.mappings"
 
