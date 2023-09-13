c0f285b462052e3c1f49f77a5ff252c210d5b85b
Hugo B. S. Benedicto <hugo.bsbenedicto@gmail.com>
2023-09-13 00:43:09 -0300

    fix(mapping): tabufline tab conflict with c-i

--- mappings.lua
+++ mappings.lua
@@ -1,5 +1,23 @@
 local M = {}
 
+M.disabled = {
+  n = {
+    ["<Tab>"] = ""
+  }
+}
+
+-- General
+
+M.general = {
+  n = {
+    -- Tab remapping conflicts with C-I key binding that jumps forwardly in the jump history
+    -- Disabling this key mapping pointing it to itself fixes this issue
+    -- Note: this solution is not universal and only works for GUI or terminals that support modifyOtherKeys
+    -- See more at https://vimhelp.org/motion.txt.html#jump-motions
+    ["<C-i>"] = { "<C-i>", { noremap = true } },
+  }
+}
+
 -- Lazy Git
 
 local OpenLazyGit = { ["<leader>lg"] = { "<cmd> LazyGit  <CR>" , "Open LazyGit" } }
@@ -36,6 +54,7 @@ M.dapui = {
 
 M.tabufline = {
   n = {
+    ["<C-Tab>"] = { "<cmd> lua require'nvchad.tabufline'.tabuflineNext() <CR>" },
     ["<leader>bm"] = { "<cmd> lua require'nvchad.tabufline'.move_buf(1) <CR>" },
     ["<leader>bM"] = { "<cmd> lua require'nvchad.tabufline'.move_buf(-1) <CR>" },
   }
