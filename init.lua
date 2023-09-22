5ead187bcfd34ee7775109259669110ee889f878
Hugo B. S. Benedicto <hugo.bsbenedicto@gmail.com>
2023-09-22 16:00:47 -0300

    refactor(init): remove refresh rate settings from neovide

--- init.lua
+++ init.lua
@@ -31,9 +31,6 @@ vim.g.neovide_padding_bottom = 0
 vim.g.neovide_padding_right = 0
 vim.g.neovide_padding_left = 0
 
-vim.g.neovide_refresh_rate = 75
-vim.g.neovide_refresh_rate_idle = 5
-
 vim.g.neovide_cursor_animate_in_insert_mode = true
 vim.g.neovide_cursor_antialiasing = true
 
