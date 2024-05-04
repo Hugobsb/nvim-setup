69e91375c84a540a12f61815e144031fc93a7d70
Hugo B. S. Benedicto <hugo.bsbenedicto@gmail.com>
2024-05-04 03:10:11 -0300

    refactor(plugins): temporarily disable rust-tools

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -543,16 +543,16 @@ return {
     config = true
   },
 
-  {
-    "simrat39/rust-tools.nvim",
-
-    ft = "rust",
-
-    dependencies = "neovim/nvim-lspconfig",
-
-    opts = function(_, opts)
-      require "rust-tools".setup(opts)
-    end
-  },
+  -- {
+  --   "simrat39/rust-tools.nvim",
+  --
+  --   ft = "rust",
+  --
+  --   dependencies = "neovim/nvim-lspconfig",
+  --
+  --   opts = function(_, opts)
+  --     require "rust-tools".setup(opts)
+  --   end
+  -- },
 }
 
