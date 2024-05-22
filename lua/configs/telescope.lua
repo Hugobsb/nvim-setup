8519dbd38a92486d8b6221e39e80aae2869c94d6
Hugo B. S. Benedicto <hugo.bsbenedicto@gmail.com>
2024-05-22 11:53:11 -0300

    refactor: use telescope default style

--- lua/configs/telescope.lua
+++ lua/configs/telescope.lua
@@ -1,4 +1,21 @@
-local config = {}
+local config = {
+  defaults = {
+    layout_strategy = "horizontal",
+    layout_config = {
+      horizontal = {
+        prompt_position = "bottom",
+        preview_width = 0.55,
+        results_width = 0.8,
+      },
+      vertical = {
+        mirror = false,
+      },
+      width = 0.87,
+      height = 0.80,
+      preview_cutoff = 120,
+    },
+  },
+}
 
 config.extensions_list = { "live_grep_args" }
 
