f4efe3747c2b006a5b467552eb082e03806dbc82
Hugo B. S. Benedicto <hugo.bsbenedicto@gmail.com>
2024-05-23 23:40:49 -0300

    refactor(config): refine session setup

--- lua/configs/obsession.lua
+++ lua/configs/obsession.lua
@@ -1,3 +1,17 @@
+# Sessions are only managed for TMUX sessions
+if os.getenv('TMUX') == nil then
+  return
+end
+
+local function load(session_file, should_create)
+  if should_create == 1 then
+    vim.cmd('silent! source ' .. session_file)
+    return
+  end
+
+  vim.fn.execute('silent! Obsession ' .. session_file)
+end
+
 local session_path = os.getenv('HOME') .. '/.local/share/nvim/sessions/'
 
 local _, session_name = xpcall(
@@ -5,23 +19,25 @@ local _, session_name = xpcall(
   function() end
 )
 
-if session_name ~= nil then
-  session_name = session_name:gsub('[^%w%.%-_]', '') .. '.vim'
+local _, window_name = xpcall(
+  function() return vim.fn.system('tmux display-message -p "#W"') end,
+  function() end
+)
+
+if session_name ~= nil and window_name ~= nil then
+  session_name = session_name:gsub('[^%w%.%-_]', '')
+  window_name = window_name:gsub('[^%w%.%-_]', '')
+
+  local session_file = session_path .. session_name .. '-' .. window_name .. '.vim'
 
   local has_session = tonumber(
     vim.fn.system(
       'cat '
-      .. session_name ..
+      .. session_file ..
       ' >/dev/null 2>&1 && echo "1" || echo "0"'
     )
   )
 
-  if has_session then
-    vim.cmd('source ' .. session_path .. session_name)
-
-    return
-  end
-
-  vim.fn.execute('Obsession ' .. session_path .. session_name)
+  load(session_file, has_session)
 end
 
