590d62e954023b2d1f21522999f6e8ba7ee5d3e7
Hugo B. S. Benedicto <hugo.bsbenedicto@gmail.com>
2024-05-27 15:38:25 -0300

    feat: add octo integration

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -648,5 +648,24 @@ return {
       close_buffers_with_windows = false,
     },
   },
+
+  {
+    "pwntester/octo.nvim",
+
+    lazy = false,
+
+    dependencies = {
+      'nvim-lua/plenary.nvim',
+      'nvim-telescope/telescope.nvim',
+      -- OR 'ibhagwan/fzf-lua',
+      'nvim-tree/nvim-web-devicons',
+    },
+
+    opts = {
+      suppress_missing_scope = {
+        projects_v2 = true,
+      }
+    },
+  },
 }
 
