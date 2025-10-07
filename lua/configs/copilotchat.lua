604463fb17ddc45c9a300f19deae96ba54e03f1a
Hugo B. S. Benedicto <hugo.bsbenedicto@gmail.com>
2025-10-07 18:19:02 -0300

    Revert "feat(copilotchat): override commit prompt for better conventional commits"
    This reverts commit 100bf310b2b62fa914c3a4d229bc84e3b095da74.

--- lua/configs/copilotchat.lua
+++ lua/configs/copilotchat.lua
@@ -9,14 +9,6 @@ local config = {
       prompt = "Based on the provided diff and on the CHANGELOG markdown file, write the latest " ..
                "changed/added/removed details using the \"Keep a Changelog\" format.",
     },
-    Commit = {
-      sticky = { '#gitdiff:staged' },
-      prompt = "Write commit message for the change with conventional commits format. " ..
-        "Keep the title under 50 characters and wrap message at 72 characters. " ..
-        "Every single line must not have more than 72 characters. " ..
-        "Format as a gitcommit code block. " ..
-        "Format separated ideas with hyphens and lines breaks."
-    },
   },
 }
 
